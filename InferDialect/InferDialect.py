c9bab32bcea7c9daf74379f0420dd95cc3a4fb5a
leimaau <229042136@qq.com>
2019-09-04 01:09:32 +0800

    更新

--- InferDialect/InferDialect.py
+++ InferDialect/InferDialect.py
@@ -24,14 +24,38 @@ for key,item in dict_dialect.items():
 # 第一步 生成中古音与方音对应数据
 
 def judge(koxqim,jyutping):
+    regstr = '([bdzgmnljiy]|dr|zr|zj|zs|zsr|zsj|gh|nr|nj|ng).*'
+    regstr2 = '([ptckqsh]|tr|cr|cj|ph|th|sr|sj|thr|ch|chr|chj|kh).*'
     if re.match(r'.*[ptk]$', koxqim) and re.match(r'.*[ptk][1-6]$', jyutping):
-        return True
+        if re.match(r''+regstr+'[ptk]$', koxqim) and re.match(r'.*[ptk][56]$', jyutping):
+            return True
+        elif re.match(r''+regstr2+'[ptk]$', koxqim) and re.match(r'.*[ptk][123]$', jyutping):
+            return True
+        else:
+            return False
     elif re.match(r'.*h$', koxqim) and re.match(r'.*[^ptk][356]$', jyutping):
-        return True
+        if re.match(r''+regstr+'h$', koxqim) and re.match(r'.*[^ptk][56]$', jyutping):
+            return True
+        elif re.match(r''+regstr2+'h$', koxqim) and re.match(r'.*[^ptk][3]$', jyutping):
+            return True
+        else:
+            return False
     elif re.match(r'.*x$', koxqim) and re.match(r'.*[^ptk][256]$', jyutping):
-        return True
+        if re.match(r''+regstr+'x$', koxqim) and re.match(r'.*[^ptk][56]$', jyutping):
+            return True
+        elif re.match(r''+regstr2+'x$', koxqim) and re.match(r'.*[^ptk][2]$', jyutping):
+            return True
+        else:
+            return False
     elif re.match(r'.*[^ptkhx]$', koxqim) and re.match(r'.*[^ptk][14]$', jyutping):
-        return True
+        if re.match(r''+regstr+'[^ptkxh]$', koxqim) and re.match(r'.*[^ptkxh][4]$', jyutping):
+            return True
+        elif re.match(r''+regstr2+'[^ptkxh]$', koxqim) and re.match(r'.*[^ptkxh][1]$', jyutping):
+            return True
+        else:
+            return False
+    elif re.match(r'.*d$', koxqim) and re.match(r'.*[36]$', jyutping):
+         return True
     else:
         return False
 
@@ -78,6 +102,7 @@ for line in data_koxqim.readlines():
     char = line.split()
     if char[1] in dict_dialect:
         continue
+        #dict_out[char[1]] = dict_koxqim_dialect[char[0]]
     else:
         try:
             if char[1] in dict_out:
@@ -85,7 +110,7 @@ for line in data_koxqim.readlines():
             else:
                 dict_out[char[1]] = dict_koxqim_dialect[char[0]]
         except:
-            dict_out[char[1]] = 'ERR'
+            dict_out[char[1]] = '_noData_'
             continue
 
 for key,item in dict_dialect.items():
