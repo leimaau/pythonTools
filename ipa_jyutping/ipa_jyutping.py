ecb876b2f9b2fd0e82a480a2946a4b9cdef36aae
leimaau <229042136@qq.com>
2019-12-15 03:01:04 +0800

    update

--- ipa_jyutping/ipa_jyutping.py
+++ ipa_jyutping/ipa_jyutping.py
@@ -18,7 +18,7 @@ for paragraph in in_txt.readlines():
         #out_txt.write(prose)
         #out_txt.write('\t')
 
-        s = ipa_to_jyutping(prose,'p')   # area: n-南宁型ipa  g-广州型ipa  p-南宁平话型ipa  p2-第二种南宁平话型ipa
+        s = ipa_to_jyutping(prose,'p')   # area: n-南宁ipa  g-广州ipa  p-南宁平话《广西通志·汉语方言志》版IPA  p2-《南宁平话词典》版IPA
         #s = jyutping_to_ipa(prose,'p',1)
         out_txt.write(s+'\n')
         
